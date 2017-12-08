6fa5cc24f3205766d11961d0e454842e843e5f9b
Joris van Vugt <jorisvanvugt@gmail.com>
2017-12-08 11:09:44 +0100

    Add exec solution

--- day8.py
+++ day8.py
@@ -1,21 +1,16 @@
 from collections import defaultdict
 import re
 
-
 def a(instructions):
-    variables = defaultdict(int)
     for line in instructions:
-        var, what, amount, _, var2, op, num = line.split(' ')
-        if var not in variables:
-            variables[var] = 0
-        if var2 not in variables:
-            variables[var2] = 0
-        if eval(f'{variables[var2]} {op} {num}'):
-            if what == 'inc':
-                variables[var] += int(amount)
-            else:
-                variables[var] -= int(amount)
-    return max(variables.values())
+        var, _, _, _, var2, *_ = line.split(' ')
+        if var not in dir():
+            exec(f'{var} = 0')
+        if var2 not in dir():
+            exec(f'{var2} = 0')
+        if eval(line.split(' if ')[1]):
+            exec(f"{var} = {line.split(' if ')[0].replace('inc', '+').replace('dec', '-')}")
+    return max(v for v in locals().values() if isinstance(v, int))
 
 
 def b(instructions):
@@ -23,10 +18,6 @@ def b(instructions):
     max_all_time = 0
     for line in instructions:
         var, what, amount, _, var2, op, num = line.split(' ')
-        if var not in variables:
-            variables[var] = 0
-        if var2 not in variables:
-            variables[var2] = 0
         if eval(f'{variables[var2]} {op} {num}'):
             if what == 'inc':
                 variables[var] += int(amount)
